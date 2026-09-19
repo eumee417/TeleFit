424b17204b2f3cf88d0acccf89146c89691af67d
박유미 <17eumee@gmail.com>
2026-09-20 03:58:19 +0900

    fix: mixed content 에러 해결 용 route 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://15.164.236.11:3000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
